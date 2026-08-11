3fa47906c5b4517914afe09e51e2f8e31a9e4fa9
JenniferCubillasCayahuallpa <jennifer.cubillas.c@vallegrande.edu.pe>
2026-08-11 16:43:47 -0500

    chore: add Maven wrapper and gitattributes

---
+++ mvnw.cmd
@@ -0,0 +1,13 @@
+@REM ----------------------------------------------------------------------------
+@REM Licensed to the Apache Software Foundation (ASF) under one
+@REM or more contributor license agreements.
+@REM Maven Wrapper startup batch script
+@REM ----------------------------------------------------------------------------
+
+@IF "%__MVNW_ARG0_NAME__%"=="" (SET "BASE_DIR=%~dp0") ELSE (SET "BASE_DIR=%%MVNW_ARG0_NAME%%")
+
+@SET MAVEN_PROJECTBASEDIR=%BASE_DIR%
+@SET MAVEN_HOME=
+@SET M2_HOME=
+
+mvn %*
